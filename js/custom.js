c8ce7b065543c582dc4cd05523518da605f83a50
Xuming Hu <xuminghu@hotmail.com>
2024-01-28 22:42:13 +0800

    Initial Update

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($){
+
+	/************** Scroll Navigation *********************/
+	$('.navigation').singlePageNav({
+        currentClass : 'active',
+        filter: ':not(.external)'
+    });
+
+
+	/************** FlexSlider *********************/
+    $('.flexslider').flexslider({
+	    animation: "fade",
+	    directionNav: false
+	});
+
+
+    /************** Responsive Navigation *********************/
+
+	$('.menu-toggle-btn').click(function(){
+        $('.responsive-menu').stop(true,true).slideToggle();
+    });
+
+});
